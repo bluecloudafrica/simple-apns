07378430f88dc64e261ffd1f6303cfb74ba22969
Yamikani Kalinde <yamikanikalinde@gmail.com>
2020-05-06 15:41:40 +0200

    fixed config path

--- src/config/apns.php
+++ src/config/apns.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    "key_id" => env("APNS_KEY_ID", null)
+    "key_id" => env("APNS_KEY_ID", null),
+    "team_id" => env("APNS_TEAM_ID", null),
+    "app_bundle" => env("APNS_APP_BUNDLE_ID", null),
+    "key_secret" => env("APNS_KEY_SECRET", null),
 ];
